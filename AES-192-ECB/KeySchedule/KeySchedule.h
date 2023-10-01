af2bd65c034feac593acfcda70f76217b0dcf0a7
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-10-01 10:58:28 -0500

    Create KeySchedule.h

---
+++ AES-192-ECB/KeySchedule/KeySchedule.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+#include "helper.h"
+
+class KeySchedule
+{
+public:
+	
+	unsigned char expandedKey[4][6][9];
+	unsigned char roundKey[4][4][13];
+
+	void run(std::string masterKey);
+
+private:
+	Help help;
+
+	unsigned char RCon[10] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
+
+	void rotWord(unsigned char temp[4], int round);
+	void subWord(unsigned char temp[4], int round);
+	void rcon(unsigned char temp[4], int round);
+
+};
